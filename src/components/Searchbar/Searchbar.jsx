c8a1e3d23c8f99585658932025af572bab5ef9ab
Oleksii Tkachenko <zlukapraporr@gmail.com>
2022-01-23 00:50:35 +0200

    Go

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import { toast } from "react-toastify";
-import styles from "./Searchbar.module.css";
-import PropTypes from "prop-types";
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import styles from './Searchbar.module.css';
+import PropTypes from 'prop-types';
 
 function Searchbar({ onSubmit }) {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
   const handleNameChange = (event) => {
     setSearch(event.currentTarget.value.toLowerCase());
@@ -13,12 +13,12 @@ function Searchbar({ onSubmit }) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (search.trim() === "") {
-      toast.error("Введите название фильма.");
+    if (search.trim() === '') {
+      toast.error('Введите название фильма.');
       return;
     }
     onSubmit(search);
-    setSearch("");
+    setSearch('');
   };
 
   return (
@@ -29,14 +29,12 @@ function Searchbar({ onSubmit }) {
         className={styles.SearchForm}
       >
         <button type="submit" className={styles.SearchFormButton}>
-          <span className={styles.SearchFormButtonLabel}>Search</span>
+          <span className={styles.SearchFormButtonLabel}>Go</span>
         </button>
 
         <input
           className={styles.SearchFormInput}
           type="text"
-          //   autocomplete="off"
-          //   autofocus
           placeholder="Search movies"
           name="search"
           value={search}
